ea1069b33992071c53aa26635300d1f28f45d4ae
Argus-66 <jqghebri@gmail.com>
2025-02-04 20:15:46 +0530

    commit

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -9,7 +9,11 @@ import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton';
 import { createUser } from '../../lib/appwrite';
 
+import { useGlobalContext } from '../../context/GlobalProvider'
+
+
 const SignUp = () => {
+  const { setUser, setIsLoggedIn } = useGlobalContext();
   const [form, setForm] = useState({
     username: '',
     email: '',
@@ -27,7 +31,8 @@ const SignUp = () => {
     try {
       const result = await createUser(form.email, form.password, form.username)
 
-      //set it to global state
+      setUser(result);
+      setIsLoggedIn(true);
 
       router.replace('/home')
     } catch (error) {
--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -11,8 +11,10 @@ import { getAllPosts, getLatestPosts } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
 import VideoCard from '../../components/VideoCard'
 
-const Home = () => {
+import { useGlobalContext } from '../../context/GlobalProvider'
 
+const Home = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const { data: posts, refetch } = useAppwrite(getAllPosts)
   const { data: latestPosts} = useAppwrite(getLatestPosts)
 
@@ -40,10 +42,10 @@ const Home = () => {
             <View className="flex-row justify-between items-start mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome Back
+                  Welcome Back,
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  Argus
+                  {user?.username}
                 </Text>
               </View>
 
