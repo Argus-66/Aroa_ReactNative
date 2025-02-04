d16163b521521c1ba852c333ccb8a4dad4b1edd5
Argus-66 <jqghebri@gmail.com>
2025-02-04 20:07:36 +0530

    commit

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import EmptyState from '../../components/EmptyState'
-import { getUserPosts } from '../../lib/appwrite'
+import { getUserPosts, signOut } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
 import VideoCard from '../../components/VideoCard'
 
 import { useGlobalContext } from '../../context/GlobalProvider'
 import { icons } from '../../constants'
 import InfoBox from '../../components/InfoBox'
+import { router } from 'expo-router'
 
 const Profile = () => {
   const { user, setUser, setIsLoggedIn } = useGlobalContext();
@@ -17,8 +18,12 @@ const Profile = () => {
     () => getUserPosts(user.$id)
   );
 
-  const logout = () => {
+  const logout = async () => {
+    await signOut();
+    setUser(null);
+    setIsLoggedIn(false);
 
+    router.replace('/sign-in');
   }
 
 
@@ -36,12 +41,13 @@ const Profile = () => {
           <View className="w-full justify-center items-center mt-6 mb-12 px-4">
             <TouchableOpacity
               className="w-full items-end mb-10"
+              onPress={logout}
             >
               <Image
                 source={icons.logout}
                 resizeMode='contain'
                 className="w-6 h-6"
-                onPress={logout}
+                
               />
             </TouchableOpacity>
 
